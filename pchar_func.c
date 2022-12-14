266640ccca1b16a30ec9c21956d59a05d4c3772c
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-14 22:36:10 +0000

    Revert: pchar_func potential logical bug

--- pchar_func.c
+++ pchar_func.c
@@ -18,7 +18,7 @@ void pchar_func(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	if (temp->n < 32 && temp->n > 127)
+	if (temp->n < 32 || temp->n > 127)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
