41a187f362a199228b123f496599447fd9d883b0
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-14 23:09:26 +0000

    Stash --

---
+++ rotr_func.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * rotr_func - rotates elements of a stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line number of code from file read
+ */
+void rotr_func(stack_t **stack, unsigned int line_number)
+{
+	stack_t *second, *first = *stack;
+	int num;
+
+	(void) line_number;
+}
