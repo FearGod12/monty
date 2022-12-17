d1eef15b4bf609b645e7b8ff22d2efd9d6932d33
FearGod12 <onyenikechukwudi@gmail.com>
2022-12-17 11:42:24 +0000

    task 6: fix

--- sub_func.c
+++ sub_func.c
@@ -9,7 +9,7 @@
  */
 void sub_func(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
+	stack_t *top, *temp = *stack;
 	int num = 0;
 
 	(void) line_number;
@@ -19,10 +19,12 @@ void sub_func(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
+	
+	top = temp;
 	num += temp->n;
 	temp = temp->next;
 	num = temp->n - num;
 	temp->n = num;
 	*stack = temp;
+	free(top);
 }
