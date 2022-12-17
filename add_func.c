e27e4efbf3ec74ceac4ff8e6b80afad8f095a0aa
FearGod12 <onyenikechukwudi@gmail.com>
2022-12-17 15:21:26 +0000

    betty fix

--- add_func.c
+++ add_func.c
@@ -17,7 +17,6 @@ void add_func(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	
 	top = temp;
 	num += temp->n;
 	temp = temp->next;
