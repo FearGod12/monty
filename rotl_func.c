90e378955054d3117d9e2e394f65f1b8c17533a3
FearGod12 <onyenikechukwudi@gmail.com>
2022-12-17 15:14:19 +0000

    bug fix

--- rotl_func.c
+++ rotl_func.c
@@ -9,13 +9,17 @@
  */
 void rotl_func(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp, *top = *stack;
+	stack_t *temp = *stack, *top = *stack;
 
 	(void) line_number;
+	if (!stack || !(*stack) || temp->next == NULL)
+	{
+		return;
+	}
 	*stack = top->next;
-	temp = *stack;
 	while (temp->next != NULL)
 		temp = temp->next;
 	temp->next = top;
+	/*temp->prev->next = NULL;*/
 	top->next = NULL;
 }
